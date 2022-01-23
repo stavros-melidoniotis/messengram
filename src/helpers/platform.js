00975d81cc940b6dc4204c52229243fa30692dcd
stavros-melidoniotis <melidon.stavros@gmail.com>
2022-01-23 15:19:56 +0200

    Added isWin & isLinux functions

--- src/helpers/platform.js
+++ src/helpers/platform.js
@@ -1,3 +1,11 @@
 exports.isMac = () => {
     return process.platform === 'darwin'
 }
+
+exports.isWin = () => {
+    return process.platform === 'win32'
+}
+
+exports.isLinux = () => {
+    return process.platform === 'linux'
+}
